f5d77961f64102b5006bb667064aa272652d4ee3
Tarcizio B <tarcizio.barbosa@outlook.com>
2022-01-22 00:17:56 -0300

    add users routes

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,13 +1,31 @@
 import "reflect-metadata";
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
+
 import "../typeorm";
+import "../../container";
+import { AppError } from "../../errors/AppError";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "test curl" });
-});
+app.use(router);
+
+app.use(
+  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.errorMessage,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 export { app };
