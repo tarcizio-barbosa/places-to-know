df3508cd78170323593208fea8c2345b38a8f770
Tarcizio B <tarcizio.barbosa@outlook.com>
2022-01-23 23:15:58 -0300

    hide access key

--- src/utils/unsplashApi.ts
+++ src/utils/unsplashApi.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import "dotenv/config";
 
 export const unsplashApi = axios.create({
   baseURL: "https://api.unsplash.com/",
   headers: {
-    Authorization: "Client-ID PiQ9n7_CtD7i56Gd353FDhOgQLWJtsdwN_DtadFTLN4",
+    Authorization: `Client-ID ${process.env.ACCESS_KEY}`,
   },
 });
